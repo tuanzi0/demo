b36a1c998d8a6fcd293848741a3ea22ac568f68a
tuanzi0 <linyi@live.cn>
2021-01-24 02:08:57 +0800

    example 3

--- learnPython.py
+++ learnPython.py
@@ -1,3 +1,4 @@
+# 1
 # num = float(input('Enter amount:'))
 # inrate = float(input('Enter inrate:'))
 # period = int(input('Enter period:'))
@@ -10,13 +11,21 @@
 #    year += 1
 
 
-# caculate average of ints
-n = 10
-list = []
-i = 0
-print('Please enter 10 numbers:')
-while i < n:
-    list.append(int(input()))
-    i += 1
-print(list)
- 
+# 2 caculate average of ints
+# n = 10
+#list = []
+#i = 0
+#print('Please enter 10 numbers:')
+#while i < n:
+#    list.append(int(input()))
+#    i += 1
+#print(list)
+
+# 3 F to C
+fahrenheit = 0
+print('Fahrenheit Celsius')
+while fahrenheit <= 250:
+    celsius = (fahrenheit - 32) / 1.8
+    print('{:5d} {:10.2f}'.format(fahrenheit, celsius))
+    fahrenheit += 25
+
