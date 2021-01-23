50b4d4e114dd4185493ce136022d63432a1d2560
tuanzi0 <linyi@live.cn>
2021-01-24 01:54:29 +0800

    start learnning

---
+++ learnPython.py
@@ -0,0 +1,22 @@
+# num = float(input('Enter amount:'))
+# inrate = float(input('Enter inrate:'))
+# period = int(input('Enter period:'))
+# value = 0
+# year = 1
+# while year <= period:
+#    value = num * (1 + inrate)
+#    print('Year {} Rs. {:.2f}'.format(year,value))
+#    num = value
+#    year += 1
+
+
+# caculate average of ints
+n = 10
+list = []
+i = 0
+print('Please enter 10 numbers:')
+while i < n:
+    list.append(int(input()))
+    i += 1
+print(list)
+ 
